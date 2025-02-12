b1d79f928550f5b2d49d9bfd9a3a3c1156de9977
Piotr Cynowski <piotr.cynowski1991petersn@gmail.com>
2025-02-12 21:26:44 +0100

    add splash effect to bullet

--- Assets/Scripts/Bullets/BulletWithSplashDamage.cs
+++ Assets/Scripts/Bullets/BulletWithSplashDamage.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Bullets
@@ -6,23 +7,51 @@ namespace Bullets
     {
         [SerializeField] private float speed = 5f;
         [SerializeField] private float splashRadius = 2f;
+        [SerializeField] private Renderer meshRenderer;
+        [SerializeField] private ParticleSystem splashEffect;
+        private bool isMoving;
+
+        private void OnEnable()
+        {
+            isMoving = true;
+            meshRenderer.enabled = true;
+        }
+
+        private void Start()
+        {
+            var main = splashEffect.main; 
+            main.startSize = splashRadius;
+        }
 
         private void Update()
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            if (isMoving)
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
         }
 
         protected override void OnHitTarget(GameObject _target = null)
         {
+            StartCoroutine(OnHitWithDelay());
+        }
+
+        private IEnumerator OnHitWithDelay()
+        {
+            isMoving = false;
+            meshRenderer.enabled = false;
+
+            splashEffect.gameObject.SetActive(true);
+            splashEffect.Play();
+
             Collider[] hits = Physics.OverlapSphere(transform.position, splashRadius);
             foreach (Collider hit in hits)
             {
                 IDamageable toDamage = hit.GetComponent<IDamageable>();
-                if (toDamage != null)
-                {
-                    toDamage.TakeDamage(damage);
-                }
+                toDamage?.TakeDamage(damage);
             }
+
+            yield return new WaitForSeconds(1);
             ReturnToPool();
         }
     }
